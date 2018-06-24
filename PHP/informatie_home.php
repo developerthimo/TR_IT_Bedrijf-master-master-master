36b24c91b5c1a1720fc11befb5415559d671e062
thimobl <thimo2000@hotmail.nl>
2018-06-24 16:51:36 +0200

    nieuw

--- PHP/informatie_home.php
+++ PHP/informatie_home.php
@@ -7,8 +7,12 @@
           </div>
           <h5>Op deze pagina vind u de portfolio`s van de medewerkers.</h5>
     <div class="row">
-    <div class="col-6"><strong>Portfolio Rutger</strong></col></div>
-    <div class="col-6"><strong>Portfolio Thimo</strong></col></div>
+    <div class="col-6"><strong>Portfolio Rutger</strong>
+      <p class="port_rutger">Ik ben een beginnende leerling van de opleiding Game- & Web development op het MBO-Utrecht. Ik werd heel snel aangetrokken door deze opleiding. Het maken van websites is een droombaan voor mij. Zowel front-end als back-end vind ik fantastisch om mee te werken. Omdat ik nog in het eerste jaar zit, heb ik nog veel te leren. In het eerste jaar hebben we al veel meegekregen over CSS, Javascript, HTML en PHP. Ook zijn we bezig geweest met een extra leerprogramma genaamd Lynda.com. Door deze opleiding hebben we de mogelijkheid om onze eigen kan van coderen kunnen kiezen.</p>
+    </div>
+    <div class="col-6"><strong>Portfolio Thimo</strong>
+      <p class="port_thimo">Ik ben een eerste jaars student aan de opleiding Media- en applicatieontwikkelaar op het MBO-Utrecht. Ik voelde mij het meeste aangetrokken tot deze school omdat het een van de weinige scholen is waar ze echt naar je kijken als individu en niet als nummer. Ook sprak deze opleiding mij erg aan omdat ik in mijn vrije tijd al het een  en ander erover opgezocht had en ik het erg leuk vond om te coderen en er meer over te leren. In het eerste jaar heb ik al een hoop geleerd zoals CSS, HTML, PHP, Javascript en zelfs een stukje TimeScript. Door mijn zelfstudie voort te zetten met behulp van Lynda.com ben ik nu ook de taal CSharp aan het leren. In de toekomst hoop ik als backend developper door te gaan en meerdere coretalen te hebben geleerd.</p>
+    </div>
     </div>
     <div class="card-footer"> 
     </div>

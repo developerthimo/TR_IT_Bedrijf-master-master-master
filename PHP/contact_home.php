dd21a66c1522715107b3ca04226182776a601963
thimobl <thimo2000@hotmail.nl>
2018-06-24 16:58:26 +0200

    iets

--- PHP/contact_home.php
+++ PHP/contact_home.php
@@ -4,7 +4,44 @@
     </div>
     <class="card text-center">
         <div class="card-content">
-          </div>
+          </div> 
+    <div class="row">
+    <div class="col-12"><strong>Hiermee kunt u ons bereiken.</strong></col></div>
+    </div>
+    &ensp;
+    <div class="row">
+        <div class="col-6">
+          <address>
+            <strong>Contact Rutger</strong>
+            <br>Columbuslaan 540
+            <br>3526 EP Utrecht
+            <br>
+          </address>
+          <address>
+            <abbr title="Phone">P:</abbr>
+            06-57773861
+            <br>
+            <abbr title="Email">E:</abbr>
+            <a href="mailto:#">318142@student.mboutrecht.nl</a>
+          </address>
+        </div></col>
+
+        <div class="col-6">
+          <address>
+            <strong>Contact Thimo</strong>
+            <br>Columbuslaan 540
+            <br>3526 EP Utrecht
+            <br>
+          </address>
+          <address>
+            <abbr title="Phone">P:</abbr>
+            06-56372676
+            <br>
+            <abbr title="Email">E:</abbr>
+            <a href="mailto:#">318526@student.mboutrecht.nl</a>
+          </address>
+        </div>
+      </div></col>
     <div class="card-footer"> 
     </div>
 </main>
--- PHP/navigatie.php
+++ PHP/navigatie.php
@@ -1,7 +1,7 @@
 <!--navigation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="Main_navbar">
       <div class="container">
-        <a class="navbar-brand" href="#">T & R</a>
+        <a class="navbar-brand" href="./index.php?action=home">T & R</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
